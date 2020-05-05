842e1896ced0624215a5278f7d712dfc5d89bce2
mastergu <mastergu@mastergudeMacBook-Pro.local>
2020-05-05 21:49:13 +0800

    [add] camera类

---
+++ Glitter/Sources/Camera.c
@@ -0,0 +1,8 @@
+//
+//  Camera.c
+//  Bullet2FileLoader
+//
+//  Created by mastergu on 2020/5/5.
+//
+
+#include "Camera.h"
---
+++ Glitter/Sources/Camera.h
@@ -0,0 +1,13 @@
+//
+//  Camera.h
+//  Bullet2FileLoader
+//
+//  Created by mastergu on 2020/5/5.
+//
+
+#ifndef Camera_h
+#define Camera_h
+
+#include <stdio.h>
+
+#endif /* Camera_h */
